dab0d9c6ef52e1dd4d3b28da169842e24728171c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-15 21:42:48 +0300

    Updated-3.

--- Program.cs
+++ Program.cs
@@ -17,18 +17,13 @@ namespace dzshka
         public C_Pole()
         {
             x_chornaya_peshka = 10;
-            x_belaya_peshka = 10;
+            x_belaya_peshka = 1;
 
         }
         public C_Pole(int _x_chornaya_peshka, int _x_belaya_peshka)
         {
             x_chornaya_peshka = _x_chornaya_peshka;
             x_belaya_peshka = _x_belaya_peshka;
-        }
-        public void show()
-        {
-            Console.Write(x_chornaya_peshka);
-            Console.Write(x_belaya_peshka);
         }
 
         public void setPosX_chornaya_peshka(int _x_chornaya_peshka)
@@ -67,25 +62,37 @@ namespace dzshka
             chornaya_peshka = _chornaya_peshka;
             belaya_peshka = _belaya_peshka;
         }
-        /*public void show()
-        {
-            Console.Write(" ");
-        }*/
 
         C_Pole pole = new C_Pole();
 
-        void I_Dvizheniye_beloy_peshki.Move_belaya_peshka()
+        void I_Dvizheniye_chernoy_peshki.Move_chornaya_peshka()
         {
-            pole.setPosX_chornaya_peshka(pole.getPosX_chornaya_peshka() - 2);
-            Console.WriteLine("Походил чёрной пешкой!");
-            Console.WriteLine(pole.getPosX_chornaya_peshka());
+            if ((pole.getPosX_chornaya_peshka() - 1) < pole.getPosX_belaya_peshka())
+            {
+                Console.WriteLine("Чёрный выиграл!");
+            }
+            else
+            {
+                pole.setPosX_chornaya_peshka(pole.getPosX_chornaya_peshka() - 2); // 8 6 4 2
+                pole.setPosX_belaya_peshka(pole.getPosX_belaya_peshka() + 2); // 3 5 7 9
+                Console.Write("Походил чёрной пешкой! - ");
+                Console.WriteLine(pole.getPosX_chornaya_peshka());
+            }
         }
 
-        public void Move_chornaya_peshka()
+        void I_Dvizheniye_beloy_peshki.Move_belaya_peshka()
         {
-            pole.setPosX_belaya_peshka(pole.getPosX_belaya_peshka() - 2);
-            Console.WriteLine("Походил белой пешкой!");
-            Console.WriteLine(pole.getPosX_belaya_peshka());
+            if ((pole.getPosX_belaya_peshka() + 1) > pole.getPosX_chornaya_peshka() && (pole.getPosX_chornaya_peshka() - 1) < pole.getPosX_belaya_peshka())
+            {
+                Console.WriteLine("Белый выиграл!");
+            }
+            else
+            {
+                pole.setPosX_belaya_peshka(pole.getPosX_belaya_peshka() + 2); // 3 5 7 9
+                pole.setPosX_chornaya_peshka(pole.getPosX_chornaya_peshka() - 2); // 8 6 4 2
+                Console.Write("Походил белой пешкой! - ");
+                Console.WriteLine(pole.getPosX_belaya_peshka());
+            }
         }
 
         public int chornaya_peshka;
@@ -96,13 +103,16 @@ namespace dzshka
     {
         static void Main(string[] args)
         {
-            C_Figura figura1 = new C_Figura();
             I_Dvizheniye_beloy_peshki belaya_peshka_1 = new C_Figura();
             I_Dvizheniye_chernoy_peshki chernaya_peshka_1 = new C_Figura();
 
             belaya_peshka_1.Move_belaya_peshka();
             chernaya_peshka_1.Move_chornaya_peshka();
-
+            belaya_peshka_1.Move_belaya_peshka();
+            chernaya_peshka_1.Move_chornaya_peshka();
+            belaya_peshka_1.Move_belaya_peshka();
+            chernaya_peshka_1.Move_chornaya_peshka();
+            belaya_peshka_1.Move_belaya_peshka();
         }
     }
 }
