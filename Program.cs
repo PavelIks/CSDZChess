97ca783b9b73958bc84335bec6e242e12dd521b0
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-15 19:17:47 +0300

    Updated-1.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace dzshka
     {
         void Move()
         {
+            C_Pole pole;
+            pole.setPosX(pole.getPosX() - 1);
             Console.WriteLine("Походил белой пешкой!");
+            Console.WriteLine(pole.getPosX());
+        }
+    }
+
+    interface I_Dvizheniye_chernoy_peshki
+    {
+        void Move()
+        {
+            C_Pole pole;
+            pole.setPosY(pole.getPosY() + 1);
+            Console.WriteLine("Походил чёрной пешкой!");
+            Console.WriteLine(pole.getPosY());
         }
     }
 
@@ -14,35 +28,62 @@ namespace dzshka
     {
         public C_Pole()
         {
-
+            x = 10;
+            y = 10;
+        }
+        public C_Pole(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
         }
         public void show()
         {
-            Console.Write(" ");
+            Console.Write(x + " : " + y);
         }
+
+        public void setPosX(int _x)
+        {
+            x = _x;
+        }
+        public void setPosY(int _y)
+        {
+            x = _y;
+        }
+
+        public int getPosY()
+        {
+            return y;
+        }
+
+        public int getPosX()
+        {
+            return x;
+        }
+        public int x;
+        public int y;
     }
 
-    class C_Figura : I_Dvizheniye_beloy_peshki
+    class C_Figura : I_Dvizheniye_beloy_peshki, I_Dvizheniye_chernoy_peshki
     {
         public C_Figura()
         {
-            chornaya_peshka = 1;
-            belaya_peshka = 1;
+            chornaya_peshka = 0;
+            belaya_peshka = 0;
         }
         public C_Figura(int _chornaya_peshka, int _belaya_peshka)
         {
             chornaya_peshka = _chornaya_peshka;
             belaya_peshka = _belaya_peshka;
         }
-        public void show()
+        /*public void show()
         {
             Console.Write(" ");
-        }
+        }*/
         public int chornaya_peshka;
         public int belaya_peshka;
     }
 
-    class C_Igra
+    /*class C_Igra
     {
         public C_Igra()
         {
@@ -52,24 +93,17 @@ namespace dzshka
         {
             Console.Write(" ");
         }
-    }
+    }*/
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введи W: ");
-            string selection = Console.ReadLine();
-            switch (selection)
-            {
-                case "W":
-                    I_Dvizheniye_beloy_peshki belaya_peshka_1 = new C_Figura();
-                    belaya_peshka_1.Move();
-                    break;
-                default:
-                    Console.WriteLine("404");
-                    break;
-            }
+            I_Dvizheniye_beloy_peshki belaya_peshka_1 = new C_Figura();
+            I_Dvizheniye_chernoy_peshki chernaya_peshka_1 = new C_Figura();
+
+            belaya_peshka_1.Move();
+            chernaya_peshka_1.Move();
         }
     }
 }
